c849f907d680dfb29391375340a71966f6288537
Jackson Horton <jacksonhorton1212@gmail.com>
2021-10-29 22:52:54 -0500

    Update install-cinnxp.sh

--- install-cinnxp.sh
+++ install-cinnxp.sh
@@ -10,29 +10,20 @@ git clone https://github.com/sass/libsass.git
 cd sassc
 SASS_LIBSASS_PATH=../libsass make
 sudo SASS_LIBSASS_PATH=../libsass make install
-
-
+cd ..
 git clone https://github.com/ndwarshuis/CinnXP
-
 cd CinnXP
-
 ./compile-theme
+
+sudo mkdir /usr/share/icons/
+sudo mkdir /usr/share/themes/
 
 sudo cp -r pkg/usr/share/icons/CinnXP/ /usr/share/icons/
 sudo cp -r pkg/usr/share/themes/CinnXP-Luna/ /usr/share/themes/
 
 cd ~/
-
 git clone https://github.com/B00merang-Artwork/Windows-XP
-mv Windows-XP/ winxp-themes
+mv Windows-XP/ winxp-themes/
 git clone https://github.com/B00merang-Project/Windows-XP
-sudo cp -r Windows-XP/Windows\ XP\ Luna/metacity-1/ /usr/share/themes
-
-sudo mv /usr/shares/themes/metacity-1/ /usr/shares/themes/CinnXP-Luna/metacity-1/
-cd ~/
-sudo mkdir /usr/share/icons/
-sudo cp -r winxp-themes/ /usr/share/icons/CinnXP-Luna/
-
-
-
-read -p “Finish process now…”
+sudo cp -r Windows-XP/Windows\ XP\ Luna/metacity-1/ /usr/share/themes/CinnXP-Luna/
+sudo cp -r winxp-themes/ /usr/share/icons/
